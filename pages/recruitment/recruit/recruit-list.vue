2a1e006ce27bb6e1df9dbb02aa73a97b29741256
wanghao <512975801@qq.com>
2020-08-10 15:03:27 +0800

    初始化页面

--- pages/recruitment/recruit/recruit-list.vue
+++ pages/recruitment/recruit/recruit-list.vue
@@ -1,160 +1,88 @@
 <template>
-	<view style="height: 100%; background-color: #fff;">
-		<!-- <view> -->
-			<frame
-				ref="frame"
-				:pages="domInfo"
-				:userInfoJson="userInfo"
-				:pagesOption="optionData"
-				:currentPageStr="currentPage"
-				:shareMessageJsonOption="shareMessageJson"
-				@getNewsInfoFun="getNewsInfo"
-				@getOptionDataFun="getOptionData"
-				@initShareMessageFun="commonMethods"
-			></frame>
-		<!-- </view> -->
+	<view>
+		<a-frame :requestParamData="requestParamData" @clickSearch="clickSearch">
+			
+			<block slot="a-swiper-below">
+					<view class="line-common-inner"></view>
+			</block>
+			
+			<block slot="a-icon-list-below">
+					<view class="line-common-inner"></view>
+			</block>
+			
+		</a-frame>
+		
+		
+		
 	</view>
+	
 </template>
 <script>
 export default {
 	data() {
 		return {
-			// 页面组件信息
-			domInfo: {},
-			// url 参数信息
-			optionData: {},
-			//分享接口所需参数
-			shareMessageJson: {},
-			// 用户信息
-			userInfo: {},
-			// 页面初始信息
-			newsInfo: {},
-			// 页面路由
-			currentPage: ''
+			myBtn: "btn-text",
+			// 请求参数
+			requestParamData:{}
 		};
-	},
-	watch: {
-		newsInfo(value) {
-			this.newsInfo = value;
-		},
-		optionData(value) {
-			this.optionData = value;
-		},
-		shareMessageJson(value) {
-			this.shareMessageJson = value;
-		},
-		currentPage(value) {
-			this.currentPage = value;
-		}
 	},
 	components: {},
 	created() {},
 	onLoad(option) {
-		this.optionData = option;
+		// 测试数据
+		// let userInfo ={
+		// 	"id": "123",
+		// 	"name": "test123",
+		// 	"phone": "手机号码"
+		// }
+		
+		// this.requestParamData = {
+		// 	"id": "456",
+		// 	"name": "test456"
+		// }
+		
+		// uni.setStorageSync("userInfo",userInfo);
+		
 	},
 
 	onReady: function(res) {},
 
-	onShow() {
-		this.commonMethods();
-	},
+	onShow() {},
 	onHide() {},
 	mounted() {
-		console.log(this.optionData)
-		this.$nextTick(() => {
-			this.$refs.frame.$emit('mountedActivity');
-			this.$refs.frame.mountedActivity();
-		});
 	},
-	activated() {
-		this.$refs.frame.$emit('activatedActivity');
-		this.$refs.frame.activatedActivity();
-	},
-	onUnload() {
-		// 子组件调用父组件生命周期
-		this.$refs.frame.$emit('onUnloadActivity');
-		this.$refs.frame.onUnloadActivity();
-	},
-	onHide() {
-		// 子组件调用父组件生命周期
-		this.$refs.frame.$emit('onHideActivity');
-		this.$refs.frame.onHideActivity();
+	activated() {},
+	onUnload() {},
+	onHide() {},
+	onPullDownRefresh() {
+		console.log('onPullDownRefresh');
+		setTimeout(function() {
+			uni.stopPullDownRefresh();
+			console.log('stopPullDownRefresh');
+		}, 1000);
 	},
 	// 监听APP点击按钮 调起分享菜单
-	onNavigationBarButtonTap(val) {
-		this.$refs.frame.$emit('onNavigationBarActivity');
-		this.$refs.frame.onNavigationBarActivity(val);
-	},
+	onNavigationBarButtonTap(val) {},
 	// 点击返回按钮
-	onBackPress() {
-		this.$refs.frame.$emit('onBackPressActivity');
-		// this.$refs.frame.onBackPressActivity();
-	},
-	// 下拉刷新
-	onPullDownRefresh() {
-		this.$refs.frame.$emit('onPullDownRefreshActivity');
-		this.$refs.frame.onPullDownRefreshActivity();
-	},
-	// 上拉加载
-	onReachBottom() {
-		this.$refs.frame.$emit('onReachBottomActivity');
-		this.$refs.frame.onReachBottomActivity();
-	},
+	onBackPress() {},
 	methods: {
-		// 获取初始化数据
-		getNewsInfo(data) {
-			this.newsInfo = data;
-		},
-		// 获取最新路由参数
-		getOptionData(data) {
-			this.optionData = data;
-		},
-		commonMethods() {
-			// 用户信息
-			this.userInfo = uni.getStorageSync('userBO');
-			this.$netComm.initPages().then(val => {
-				this.domInfo = val;
-			});
+		// 点击搜索框
+		clickSearch() {
+			// this.$emit('goSearchFun');
+			uni.showToast({
+				icon:'none',
+				title:'点击了搜索框'
+			})
+			console.log('点击了搜索框');
 		}
 	}
 };
 </script>
-<style>
-page {
-	height: 100%;
-}
 
-.tab {
-	position: fixed;
-	height: 96rpx;
-	background-color: #fff;
-	display: flex;
-	line-height: 96rpx;
-	justify-content: space-around;
-	/* padding: 0 120rpx; */
-	box-shadow: 0 0 20upx #e5e5e5;
-}
-
-.tab .tab-item view {
-	padding: 0 20rpx;
-	height: 90rpx;
-	line-height: 90rpx;
-}
-
-.tab .tab-item .active text {
-	font-size: 18px;
-}
-
-.content {
-	padding: 20rpx;
-}
-
-.assign-typeinner.train .time view:first-child {
-	margin-right: 16rpx;
-}
-
-.assign-typeinner.exam .time view {
-	margin-left: 100rpx;
-}
-
+<style scoped>
+	.line-common-inner {
+		background: #f0eff5;
+		height: 20upx;
+		width: 100%;
+	}
 </style>
