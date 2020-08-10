04c344921436099e3d997fbbcca820975b65c012
wanghao <512975801@qq.com>
2020-08-10 14:52:47 +0800

    修改环境

--- framework/config/app.config.js
+++ framework/config/app.config.js
@@ -4,21 +4,21 @@ const CONFIG = {
 		tokenKey: "TOKEN_DEV" ,// 登录标识,
 		baseUrl: "http://dev.sicbcore.com",
 		// 公司ID
-		companyId:"f572a336a93e4a379987c063154cfbad",
+		companyId:"b2f38c6e747e4b2d9db341faec7c0e6a",
 		// 项目ID
-		projectId:"e67789780c42c91e287113460f774e43"
+		projectId:"63930c07614f4f45ad1638cbef6bfd8e"
 	},
-
 	//生产环境配置
 	production: {
 		tokenKey: "TOKEN_PRO",// 登录标识,
 		baseUrl: "https://jizhanlipeixun.sicbcore.com",
 		// 公司ID
-		companyId:"f572a336a93e4a379987c063154cfbad",
+		companyId:"b2f38c6e747e4b2d9db341faec7c0e6a",
 		// 项目ID
-		projectId:"e67789780c42c91e287113460f774e43"
+		projectId:"63930c07614f4f45ad1638cbef6bfd8e"
 	}
 
 }
 
-export default CONFIG[process.env.NODE_ENV];
+// export default CONFIG[process.env.NODE_ENV];
+export default CONFIG.development;
