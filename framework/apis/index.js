a8f7ff593e04e5c8853804a579b0968e2b448a36
Xulx <15621815312@163.com>
2020-08-20 15:42:33 +0800

    封装按钮业务组件

--- framework/apis/index.js
+++ framework/apis/index.js
@@ -5,10 +5,10 @@ import config from "@/framework/config/app.config.js";
 export const postLogin = (data) => http.POST(`${config.baseUrl}/api/appLogin/login`, data);
 
 // 请求低代码渲染组件信息
-// export const queryPageJson = (data) => http.POST(`${config.baseUrl}/lowcode-api/page/queryPageJson`, data);
+export const queryPageJson = (data) => http.POST(`${config.baseUrl}/lowcode-api/page/queryPageJson`, data);
 
 // 前端造数据接口
-export const queryPageJson = (data) => http.GET(`/mock/5f211e5d339f163501d503a6/lowcode/xlx`, data);
+// export const queryPageJson = (data) => http.GET(`/mock/5f211e5d339f163501d503a6/lowcode/xlx`, data);
 
 // POST请求
 // export const POST = (url,data) => http.POST(`${config.baseUrl}/${url}`, data);
