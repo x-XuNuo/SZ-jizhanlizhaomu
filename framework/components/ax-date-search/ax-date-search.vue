fe1142836312266ed63a10c5c776782e01e38d87
Xulx <15621815312@163.com>
2020-08-24 15:25:16 +0800

    点击查询按钮

--- framework/components/ax-date-search/ax-date-search.vue
+++ framework/components/ax-date-search/ax-date-search.vue
@@ -52,14 +52,16 @@ export default {
 		} 
 		this.$nextTick( ()=> {
 			let calendarOD = this.$refs.calendar.operateData
-			// let btnOD = this.$refs.btn.operateData
+			let btnOD =  this.$refs.btn.operateData
 			calendarOD.confirm = "this.propsData.titleText = e.fulldate"
+			// btnOD.click = "console.log('57:',e)"
+			// console.log("58：",btnOD)
 			// btnOD.link = "http://easy-mock.liuup.com/mock/5f36392d339f163501d50421/example/restful/list"
 			// btnOD.linkType = "1"
 			this.$refs.calendar.setPropsData(dateInfo),
 			this.$refs.calendar.setOperateData(calendarOD)
 			this.$refs.btn.setPropsData(btnInfo)			
-			// this.$refs.calendar.setOperateData(btnOD)
+			this.$refs.btn.setOperateData(btnOD)
 		})
 	},
 	
